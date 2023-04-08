f2f001ec41ddceb3f3c8cf00d969c2a6473467b9
apfurlan <alexandrepfurlan@gmail.com>
2023-04-08 02:19:52 -0300

    adding the read_input.cpp

--- read_input_file.cpp
+++ read_input_file.cpp
@@ -36,48 +36,18 @@ void read_yaml(std::string fn) {
         }
         
      } else {
-        std::cout << "It is not a sequence" << std::endl;
-        std::cout << no << std::endl ;
-        //if (no.IsNull()) {
-        //    std::cout << "Erro" << std::endl;
-        //}
-    }
-
-    //     for (auto node : no ) {
-    //         std::cout << "aawww" << std::endl;
-    //         std::cout << "Nome do nó: " << node.Tag() << std::endl;
-    // }
-        //std::string nome_campo = map_it->first.as<std::string>();
-    //     YAML::Node valor_campo = map_it->second;
-
-    //     // Imprimir o nome do campo e o valor correspondente
-    //     std::cout << nome_campo << ": " << valor_campo << std::endl;
-    
+        for (auto it = no.begin(); it != no.end(); ++it) {
+            std::string key = it->first.as<std::string>(); 
+            std::cout << "Nome da chave: " << key <<  std::endl;
+        }     //if (it.IsSequence()) {
+             //   std::cout << "é " <<  std::endl;
+             //} else {
+             //s  std::cout << "nao é " <<  std::endl;
+             //} 
 
 
-    // Processar o conteúdo do nó YAML
-    // std::string name = no["general"]["name"].as<std::string>();
-    // int L = no["geometry"]["L"].as<int>();
-    // int ndim = no["geometry"]["ndim"].as<int>();
-    // int ncoord = no["geometry"]["ncoord"].as<int>();
-    // std::string method = no["simulation"]["method"].as<std::string>();
-    // int thermalization_steps = no["simulation"]["termaliztion_steps"].as<int>();
-    // int store_averages_steps = no["simulation"]["store_averages_steps"].as<int>();
-    // int window = no["output"]["window"].as<int>();
-    // std::string output_file_1 = no["output"]["files"][0]["filename_1"].as<std::string>();
-    // int quantidade_1 = no["output"]["files"]["quantities"][0].as<std::string>();
-
-    // // Imprimir os valores lidos do arquivo YAML
-    // std::cout << "Name: " << name << std::endl;
-    // std::cout << "linear size : " << L << std::endl;
-    // std::cout << "No of dimensions: " << ndim << std::endl;
-    // std::cout << "ncoord: " << ncoord << std::endl;
-    // std::cout << "method: " << method << std::endl;
-    // std::cout << "thermalization_steps: " << thermalization_steps << std::endl;
-    // std::cout << "store_averages_steps: " << store_averages_steps << std::endl;
-    // std::cout << "output_file_1: " << output_file_1 << std::endl;
-    //std::cout << "quantidades: " << quantidade_1 << std::endl;
-    
+       
+     }
     return ; 
 }
 
@@ -85,12 +55,6 @@ int main(int argc, char* argv[])
 {
 
   std::string fn = argv[1] ;//"filename.conf";
-
-  if (fn.substr(fn.find_last_of(".") + 1) == "conf") {
-    std::cout << "Yes..." << std::endl;
-    // here the function to parse conf file
-  } else if (fn.substr(fn.find_last_of(".") + 1) == "yaml") {
-    std::cout << "It is a yaml file" << std::endl;
 
     read_yaml(fn) ;
  
@@ -109,7 +73,7 @@ int main(int argc, char* argv[])
     // std::cout << "No of dimensions: " << ndim << std::endl;
     // std::cout << "ncoord: " << ncoord << std::endl;
     //here the function to parse yaml file
-  }
+  //}
 }
 
 
@@ -161,3 +125,43 @@ int main(int argc, char* argv[])
 
 //     return 0;
 // }
+
+
+
+
+
+    //     for (auto node : no ) {
+    //         std::cout << "aawww" << std::endl;
+    //         std::cout << "Nome do nó: " << node.Tag() << std::endl;
+    // }
+        //std::string nome_campo = map_it->first.as<std::string>();
+    //     YAML::Node valor_campo = map_it->second;
+
+    //     // Imprimir o nome do campo e o valor correspondente
+    //     std::cout << nome_campo << ": " << valor_campo << std::endl;
+    
+
+
+    // Processar o conteúdo do nó YAML
+    // std::string name = no["general"]["name"].as<std::string>();
+    // int L = no["geometry"]["L"].as<int>();
+    // int ndim = no["geometry"]["ndim"].as<int>();
+    // int ncoord = no["geometry"]["ncoord"].as<int>();
+    // std::string method = no["simulation"]["method"].as<std::string>();
+    // int thermalization_steps = no["simulation"]["termaliztion_steps"].as<int>();
+    // int store_averages_steps = no["simulation"]["store_averages_steps"].as<int>();
+    // int window = no["output"]["window"].as<int>();
+    // std::string output_file_1 = no["output"]["files"][0]["filename_1"].as<std::string>();
+    // int quantidade_1 = no["output"]["files"]["quantities"][0].as<std::string>();
+
+    // // Imprimir os valores lidos do arquivo YAML
+    // std::cout << "Name: " << name << std::endl;
+    // std::cout << "linear size : " << L << std::endl;
+    // std::cout << "No of dimensions: " << ndim << std::endl;
+    // std::cout << "ncoord: " << ncoord << std::endl;
+    // std::cout << "method: " << method << std::endl;
+    // std::cout << "thermalization_steps: " << thermalization_steps << std::endl;
+    // std::cout << "store_averages_steps: " << store_averages_steps << std::endl;
+    // std::cout << "output_file_1: " << output_file_1 << std::endl;
+    //std::cout << "quantidades: " << quantidade_1 << std::endl;
+    

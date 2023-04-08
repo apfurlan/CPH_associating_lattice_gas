fbe6d4b5cd5cbc9eb583fa988027a790680b4b00
apfurlan <alexandrepfurlan@gmail.com>
2023-04-07 21:52:29 -0300

    adding the read_input.cpp

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Lattice Simulation Tools
+# **Lattice Simulation Tools**
 
 Este repositório consiste de um conjunto de classes, métodos e modelos que 
 desenvolvi ao longo dos anos que podem ajudar no desenvolvimento de códigos 
@@ -8,15 +8,24 @@ topologias já implementadas são, quadrada, triangular, cúbica e FCC.
 
 repositório contém as seguintes classes : 
 
-    - adjacency_matrix_class : esta classe gera a lista de vizinhos de um 
-    para todos os sítios da rede. Ela necessaita dos seguintes parâmetros de
-    entrada : 
-        - L : Dimensões Laterais da rede
-        - ndim : Número de dimensões da rede
-        - N : Número total de sítios da rede ->  N = L**(ndim)
-        - ncoord : número vizinhos (numero de coordenação)
-        - nn : lista de vizinhos
+- adjacency_matrix_class : esta classe gera a lista de vizinhos de um 
+para todos os sítios da rede. Ela necessaita dos seguintes parâmetros de
+entrada : 
+    - L : Dimensões Laterais da rede
+    - ndim : Número de dimensões da rede
+    - N : Número total de sítios da rede ->  N = L**(ndim)
+    - ncoord : número vizinhos (numero de coordenação)
+    - nn : lista de vizinhos
 
-    - sublattice_class : classe geradora da subrede do sistema. Nesta
-    classe identificamos quais sítios 
-            
+- sublattice_class : classe geradora da subrede do sistema. Nesta
+classe identificamos quais sítios 
+
+- read_input : este arquivo contém funções que são usadas para ler
+o arquivo de input. É aconselhável que se use um arquivo .yaml como
+input para código.             
+
+## **Compile** 
+
+To compile this code use : 
+
+- `g++ read_input_file.cpp -lyaml-cpp`
--- read_input_file.cpp
+++ read_input_file.cpp
@@ -1,47 +1,108 @@
-#include <stdio.h>
-#include <yaml.h>
-#include <assert.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <yaml-cpp/yaml.h>
 
-int main()
-{
-    FILE *file;
-    yaml_parser_t parser;
-    yaml_document_t document;
-    yaml_node_t *node;
-    int i = 1;
+void read_yaml(std::string filename) ;
 
-    file = fopen("input_model.yaml", "rb");
-    assert(file);
+void read_yaml(std::string fn) {
 
-    assert(yaml_parser_initialize(&parser));
+    // std::ifstream input_file("input_model.yaml");
+    std::ifstream input_file(fn);
 
-    yaml_parser_set_input_file(&parser, file);
+    //Ler o conteúdo do arquivo YAML em um nó YAML
+    YAML::Node no = YAML::Load(input_file);
 
-    if (!yaml_parser_load(&parser, &document)) {
-        goto done;
-    }
+    // Processar o conteúdo do nó YAML
+    std::string name = no["name"].as<std::string>();
+    int ndim = no["ndim"].as<int>();
+    int ncoord = no["ncoord"].as<int>();
 
-    // iterate through each node
-    while(1) {
-        node = yaml_document_get_node(&document, i);
-        if(!node) break;
-        if(node->type == YAML_SCALAR_NODE) {
-            if (node->data.scalar.style == 1){ //assuming that the key is a string
-                printf("%s: ", node->data.scalar.value);
-                i++; 
-                node = yaml_document_get_node(&document, i); //assuming that the value is stored as the next node
-                if(!node) break;
-                printf("%s (%d)\n", node->data.scalar.value, node->data.scalar.style); //value for each key and the type of value in the braces
-            }
-        }
-        i++;
-    }
-    yaml_document_delete(&document);
+    // Imprimir os valores lidos do arquivo YAML
+    std::cout << "Name: " << name << std::endl;
+    std::cout << "No of dimensions: " << ndim << std::endl;
+    std::cout << "ncoord: " << ncoord << std::endl;
 
-
-    done:
-      yaml_parser_delete(&parser); //to free memory
-      assert(!fclose(file));
-
-    return 0;
+    return ; 
 }
+
+int main(int argc, char* argv[])
+{
+
+  std::string fn = argv[1] ;//"filename.conf";
+
+  if (fn.substr(fn.find_last_of(".") + 1) == "conf") {
+    std::cout << "Yes..." << std::endl;
+    // here the function to parse conf file
+  } else if (fn.substr(fn.find_last_of(".") + 1) == "yaml") {
+    std::cout << "It is a yaml file" << std::endl;
+
+    read_yaml(fn) ;
+ 
+    // std::ifstream input_file("input_model.yaml");
+
+    // //Ler o conteúdo do arquivo YAML em um nó YAML
+    // YAML::Node no = YAML::Load(input_file);
+
+    // // Processar o conteúdo do nó YAML
+    // std::string name = no["name"].as<std::string>();
+    // int ndim = no["ndim"].as<int>();
+    // int ncoord = no["ncoord"].as<int>();
+
+    // // Imprimir os valores lidos do arquivo YAML
+    // std::cout << "Name: " << name << std::endl;
+    // std::cout << "No of dimensions: " << ndim << std::endl;
+    // std::cout << "ncoord: " << ncoord << std::endl;
+    //here the function to parse yaml file
+  }
+}
+
+
+// void read_yaml(std::string fn) {
+
+//     // std::ifstream input_file("input_model.yaml");
+//     std::ifstream input_file(fn);
+
+//     //Ler o conteúdo do arquivo YAML em um nó YAML
+//     YAML::Node no = YAML::Load(input_file);
+
+//     // Processar o conteúdo do nó YAML
+//     std::string name = no["name"].as<std::string>();
+//     int ndim = no["ndim"].as<int>();
+//     int ncoord = no["ncoord"].as<int>();
+
+//     // Imprimir os valores lidos do arquivo YAML
+//     std::cout << "Name: " << name << std::endl;
+//     std::cout << "No of dimensions: " << ndim << std::endl;
+//     std::cout << "ncoord: " << ncoord << std::endl;
+
+//     return ; 
+// }
+
+
+
+    // Verificar se o nó é uma sequência (ou seja, um vetor)
+//     if (no.IsSequence())
+//     {
+//         // Percorrer todos os elementos da sequência
+//         for (YAML::iterator it = no.begin(); it != no.end(); ++it)
+//         {
+//             // Verificar se o elemento é um mapa (ou seja, um dicionário)
+//             if (it->IsMap())
+//             {
+//                 // Percorrer todos os campos do mapa
+//                 for (YAML::const_iterator map_it = it->begin(); map_it != it->end(); ++map_it)
+//                 {
+//                     // Obter o nome do campo e o valor correspondente
+//                     std::string nome_campo = map_it->first.as<std::string>();
+//                     YAML::Node valor_campo = map_it->second;
+
+//                     // Imprimir o nome do campo e o valor correspondente
+//                     std::cout << nome_campo << ": " << valor_campo << std::endl;
+//                 }
+//             }
+//         }
+//     }
+
+//     return 0;
+// }
